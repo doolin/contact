1c48ecf923c4722d1bf2f51b954f8175270a01bb
Dave Doolin <david.doolin@gmail.com>
2010-11-17 10:38:05 -0800

    Added step defs for the features file.

---
+++ features/step_definitions/sendmessage_steps.rb
@@ -0,0 +1,13 @@
+
+
+Given /^there's a message sending form$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^the user sends a message$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should get email$/ do
+  pending # express the regexp above with the code you wish you had
+end
