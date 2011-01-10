60f1ea203e4361df59d7c67ad99e049dd97be986
Dave Doolin <david.doolin@gmail.com>
2011-01-09 17:48:10 -0800

    Removed the signup link from the sign in page.

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,15 @@
+class RegistrationsController < Devise::RegistrationsController
+  def new
+    redirect_to root_path
+  end
+
+=begin
+  def create
+    # add custom create logic here
+  end
+
+  def update
+    super
+  end
+=end
+end 
