6e1e50eb814a1558c7f6a3b46e9d48cb58f9367c
Dave Doolin <david.doolin@gmail.com>
2011-06-20 09:15:31 -0700

    Updated cucumber.

--- Gemfile
+++ Gemfile
@@ -4,29 +4,29 @@ gem 'rails', '3.0.7'
 gem 'sqlite3-ruby', :require => 'sqlite3'
 gem 'devise'
 gem 'dynamic_form'
-gem 'dalli'
-gem "builder", "~> 2.1.2"
+#gem 'dalli'
+#gem "builder", "~> 2.1.2"
 
 group :cucumber do
   gem 'capybara'
   gem 'database_cleaner'
-  gem 'cucumber-rails'
+  gem 'cucumber-rails' 
   gem 'cucumber'
-  gem 'spork'
-  gem 'launchy'    # So you can do Then show me the page
+#  gem 'spork'
+#  gem 'launchy'    # So you can do Then show me the page
 end  
 
 group :development do
-  gem 'wrong'
+#  gem 'wrong'
   gem 'rspec-rails'
-  gem 'annotate-models', '1.0.4'
+#  gem 'annotate-models', '1.0.4'
 end
 
 group :test do
-  gem 'rspec'
+#  gem 'rspec'
   gem 'rspec-rails'
-  gem 'webrat', '0.7.2'
-  gem 'factory_girl_rails', '1.0'
+#  gem 'webrat', '0.7.2'
+  gem 'factory_girl_rails'
   gem 'email_spec' 
 #  gem 'cover_me', '>= 1.0.0.rc2'
 end
--- features/support/paths.rb
+++ features/support/paths.rb
@@ -8,21 +8,9 @@ module NavigationHelpers
   def path_to(page_name)
     case page_name
 
-    when /the home\s?page/
+    when /^the home\s?page$/
       '/'
 
-    when /the admin's sign_in page/
-      '/login'
-      
-    when /Send me an email!/
-      '/email_contacts'
-
-    when /the email contacts page/
-      '/email_contacts'
-      
-    when /Thank you/
-      '/thankyou'
-      
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
@@ -31,10 +19,10 @@ module NavigationHelpers
 
     else
       begin
-        page_name =~ /the (.*) page/
+        page_name =~ /^the (.*) page$/
         path_components = $1.split(/\s+/)
         self.send(path_components.push('path').join('_').to_sym)
-      rescue Object => e
+      rescue NoMethodError, ArgumentError
         raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
           "Now, go and add a mapping in #{__FILE__}"
       end
