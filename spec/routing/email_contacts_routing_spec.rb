4b60219ca06f0898427428d5519ad30dab8ce8b6
David Doolin <david.doolin@gmail.com>
2017-05-06 08:08:34 -0700

    rails41 - fixed some pending specs
    For whatever reason, some specs which were not working
    and moved to pending are now working again. Very weird,
    but I'll take it.
    
    There is almost surely more to do with the application
    update task, but it will take a bit of focus on that
    specifically, and should be in an upcoming commit.

--- spec/routing/email_contacts_routing_spec.rb
+++ spec/routing/email_contacts_routing_spec.rb
@@ -5,31 +5,31 @@ require 'spec_helper'
 # describe EmailContactsController do
 describe 'routes for email contacts controller' do
   describe 'routing', :type => :routing do
-    xit 'recognizes and generates #index' do
+    it 'recognizes and generates #index' do
       expect(get: '/email_contacts').to route_to(controller: 'email_contacts', action: 'index')
     end
 
-    xit 'recognizes and generates #new' do
+    it 'recognizes and generates #new' do
       { get: '/email_contacts/new' }.should route_to(controller: 'email_contacts', action: 'new')
     end
 
-    xit 'recognizes and generates #show' do
+    it 'recognizes and generates #show' do
       { get: '/email_contacts/1' }.should route_to(controller: 'email_contacts', action: 'show', id: '1')
     end
 
-    xit 'recognizes and generates #edit' do
+    it 'recognizes and generates #edit' do
       { get: '/email_contacts/1/edit' }.should route_to(controller: 'email_contacts', action: 'edit', id: '1')
     end
 
-    xit 'recognizes and generates #create' do
+    it 'recognizes and generates #create' do
       { post: '/email_contacts' }.should route_to(controller: 'email_contacts', action: 'create')
     end
 
-    xit 'recognizes and generates #update' do
+    it 'recognizes and generates #update' do
       { put: '/email_contacts/1' }.should route_to(controller: 'email_contacts', action: 'update', id: '1')
     end
 
-    xit 'recognizes and generates #destroy' do
+    it 'recognizes and generates #destroy' do
       { delete: '/email_contacts/1' }.should route_to(controller: 'email_contacts', action: 'destroy', id: '1')
     end
   end
