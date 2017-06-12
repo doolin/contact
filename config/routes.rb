dec4c787d541b9fec3d6b5ad186e2278d6569358
David Doolin <david.doolin@gmail.com>
2017-06-12 15:45:19 -0700

    cucumber: devise logging in and out work
    The `devise_scope` needed `admin` not `admins`.
    Remember: scope implies model.

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 # frozen_string_literal: true
 
 Contact::Application.routes.draw do
+  # This route is for eliminating the default devise sign up
   devise_for :admins
+  # devise_for :admins, controllers: { registrations: 'admin/registrations' }
 
   resources :email_contacts
   get '/thankyou', to: 'email_contacts#thankyou'
   root to: 'email_contacts#new'
 
-  # This route is for eliminating the default devise sign up
-  # devise_for :admins, controllers: { registrations: 'admin/registrations' }
-
-  devise_scope :admins do
+  devise_scope :admin do
     get '/login' => 'devise/sessions#new'
     get '/logout' => 'devise/sessions#destroy'
   end
