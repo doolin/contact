545109970f348a16f9e06cded7991035ee0a1b7e
David Doolin <david.doolin@gmail.com>
2017-06-13 15:45:32 -0700

    cucumber: get rid of some web steps
    Not just a few, probably half the steps
    have been deleted in this commit.
    
    Wasn't able to eliminate the `web_steps.rb`
    file completely yet. Hopefully soon.
    
    This is now mergeable.

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -7,116 +7,36 @@ Given /^the following email_contacts exist:$/ do |contacts_table|
   contacts_table.hashes.each do |hash|
     EmailContact.create!(hash)
   end
-  # binding.pry
 end
 
-# From the Devise wiki:
 Given /^I am not authenticated$/ do
-  # visit('/admins/sign_out') # ensure that at least
-  visit('/logout') # ensure that at least
-end
-
-Given /^I have one\s+user "([^\"]*)" with password "([^\"]*)" and login "([^\"]*)"$/ do |email, password, login|
-  User.new(email: email,
-           login: login,
-           password: password,
-           password_confirmation: password).save!
-end
-
-Given /^I have one\s+admin "([^\"]*)" with password "([^\"]*)" and login "([^\"]*)"$/ do |email, password, login|
-  Admin.new(email: email,
-           password: password,
-           password_confirmation: password).save!
-end
-
-Given /^I am a new, authenticated user$/ do
-  email = 'testing@man.net'
-  login = 'Testing man'
-  password = 'secretpass'
-
-  Given %{I have one user "#{email}" with password "#{password}" and login "#{login}"}
-  And %{I go to login}
-  And %{I fill in "user_email" with "#{email}"}
-  And %{I fill in "user_password" with "#{password}"}
-  And %{I press "Sign in"}
+  visit('/logout')
 end
 
 Given /^I am an authenticated admin$/ do
   email = 'testing@man.net'
-  login = 'Testing man'
   password = 'secretpass'
   Admin.new(email: email,
            password: password,
            password_confirmation: password).save!
-
-  FactoryGirl.create :email_contact
-
   @email_contacts = EmailContact.all
 
-
   visit path_to("the admin's sign_in page")
-
-  # binding.pry
-
   fill_in("admin_email", with: email)
   fill_in("admin_password", with: password)
   click_button("Log in")
-
-  # puts "HERE!!!"
-
-  # Given %{I have one admin "#{email}" with password "#{password}" and login "#{login}"}
-  # And %{I go to the admin's sign_in page} # And %{I go to login}
-  # And %{I fill in "admin_email" with "#{email}"}
-  # And %{I fill in "admin_password" with "#{password}"}
-  # And %{I press "Sign in"}
 end
 
-# This runs, but I don't think it does anything
-#
-#   before(:each) do
-#     @admin = Admin.create("13")
-#     sign_in(:admin,@admin)
-#   end
-
-# Given /^I fill in the "([^"]*)" with "([^"]*)"$/ do |arg1, arg2|
-#  pending # express the regexp above with the code you wish you had
-# end
-
-# This isn't quite right, should use an already created admin
-# and log that admin in.
 Then /^I should land on the "Send me an email!" page$/ do
+  # TODO: add an assertion here.
 end
 
-# This is currently failing because there are no emails
-# in the email_contacts list.
 Given /^I click on the email "Delete" link$/ do
-  # save_and_open_page
-  # first('Delete').click_link('Delete')
-
   deletes = all("a", :text => 'Delete')
-
   deletes.first.click
-
-  # binding.pry
-
-  # click_link('Delete')
-
-  #  pending # express the regexp above with the code you wish you had
 end
 
 Then /^the email is deleted$/ do
-  pending # express the regexp above with the code you wish you had
-end
-
-Given /^not logged in user on any page$/ do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then /^the "([^"]*)" link should not be displayed$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
-
-Then /^the user should be routed to index page$/ do
   pending # express the regexp above with the code you wish you had
 end
 
--- features/step_definitions/sendmessage_steps.rb
+++ features/step_definitions/sendmessage_steps.rb
@@ -1,36 +1,7 @@
-
-
 # frozen_string_literal: true
 
 Given /^I'm on the message sending form page$/ do
   visit('/')
-  # pending # express the regexp above with the code you wish you had
-end
-
-# Given /^there's a message sending form web page$/ do
-#  pending # express the regexp above with the code you wish you had
-# end
-
-When /^the user sends a message$/ do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then /^I should get email$/ do
-  pending # express the regexp above with the code you wish you had
-end
-
-# Given /^I fill in the "([^"]*)" field$/ do |value|
-#  fill_in(field, :with => value)
-#  #pending # express the regexp above with the code you wish you had
-# end
-
-Then /^Dave should get my email$/ do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then /^I should see the "([^"]*)" page$/ do |arg1|
-  # This should be simple, some webrat code I think.
-  pending # express the regexp above with the code you wish you had
 end
 
 # TODO: the regex is problematic here, I should figure
@@ -40,5 +11,3 @@ Then /^I land on the "Thank You" page$/ do
   current_path = URI.parse(current_url).path
   expect(current_path).to eq '/thankyou'
 end
-
-
